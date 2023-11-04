6ba3d57bd00780ef9b2f06548a78678862a9ef9e
yankee <tad.training2023@gmail.com>
2023-11-04 23:24:21 +0900

    ex6電卓をつくろう

--- APG4b/ex2/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    cout << "いつも" << 2525 << endl;
-    cout << "AtCoderくん" << endl;
-}
--- APG4b/ex3/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  cout << 0.5*100*(100+1) << endl;
-}
