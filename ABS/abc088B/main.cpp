3c753d3f968f3132906dca9fd5e16cfc718b43b8
yankee <tad.training2023@gmail.com>
2023-11-12 17:07:47 +0900

    コメント追記

--- ABS/abc088B/main.cpp
+++ ABS/abc088B/main.cpp
@@ -11,11 +11,14 @@ int main() {
         cin >> a[i];
     }
     
+    // 昇順にソート
     sort(a.begin(), a.end());
+    
     int alice = 0, bob = 0;
     for (int i = 0; i < n; i++) {
+        // 大きい方から取っていく
         int point = a[n-1-i];
-        // 偶数奇数でどちらへの得点か決まる
+        // パリティ(偶数奇数)分けでどちらへの得点か決まる
         if (0 == i % 2) {
             alice += point;
         } else if (1 == i % 2) {
