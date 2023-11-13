04a17feac007a9cc5f0c3b28f18ce1a25e496de3
yankee <tad.training2023@gmail.com>
2023-11-13 19:28:10 +0900

    ロジック変更
    1つ目は必ずOKなのでforループを1からにした

--- ABS/abc085B/main.cpp
+++ ABS/abc085B/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    int n, ans = 0, pre_d = 0;
+    int n, ans = 1, pre_d = 0;
     cin >> n;
     vector<int> d(n);
     for (int i = 0; i < n; i++) {
@@ -14,21 +14,17 @@ int main() {
     // 降順にソート
     sort(d.begin(), d.end(), greater<int>());
 
-    // 大きい方から取り出し
-    // 一つ前に取り出したものと同じ大きさの場合は何もせずにスキップ
-    for (int i = 0; i < n; i++) {
-        if (0 == i) {
+    // 1つ目はOKなので、の初期値は1
+    // 一つ前を表すpre_dに0indexを代入
+    pre_d = d[0];
+
+    // 一つ前に取り出したものより小さいときだけ加算
+    for (int i = 1; i < n; i++) {
+        if (d[i] < pre_d) {
             pre_d = d[i];
             ans++;
-            continue;
-        } else {
-            if (d[i] == pre_d) {
-                continue;
-            } else {
-                pre_d = d[i];
-                ans++;
-            }
         }
     }
     cout << ans << endl;
+    return 0;
 }
