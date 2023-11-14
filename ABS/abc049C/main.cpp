e3520e1db0e0ab13f5c6ae1e90a127965cccea9f
yankee <tad.training2023@gmail.com>
2023-11-14 18:47:39 +0900

    abc049C - 白昼夢
    ACになった

--- ABS/abc049C/main.cpp
+++ ABS/abc049C/main.cpp
@@ -6,12 +6,19 @@ int main() {
     cin >> S;
     int len = S.length();
     int index = 0;
+    // 文字列が紛らわしいものから比較する
     while (true) {
-        if (S.substr(index, 5) == "dream") {
-            index += 5;
+        if (S.substr(index, 11) == "dreameraser") {
+            index += 11;
+            continue;
+        } else if (S.substr(index, 10) == "dreamerase") {
+            index += 10;
             continue;
         } else if (S.substr(index, 7) == "dreamer") {
             index += 7;
+            continue;
+        } else if (S.substr(index, 5) == "dream") {
+            index += 5;
             continue;
         } else if (S.substr(index, 6) == "eraser") {
             index += 6;
@@ -19,7 +26,7 @@ int main() {
         } else if (S.substr(index, 5) == "erase") {
             index += 5;
             continue;
-        } else if (len <= index) {
+        } else if (len == index) {
             break;
         } else {
             cout << "NO" << endl;
@@ -28,6 +35,5 @@ int main() {
     }
     
     cout << "YES" << endl;
-    
     return 0;
 }
