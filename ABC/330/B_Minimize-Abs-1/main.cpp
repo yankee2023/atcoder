7cdd63e767b5278677b3f2d015e9165dd2a0f227
yankee <tad.training2023@gmail.com>
2023-11-26 15:34:56 +0900

    330やり直し

--- ABC/330/B_Minimize-Abs-1/main.cpp
+++ ABC/330/B_Minimize-Abs-1/main.cpp
@@ -1,23 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int N, L, R, ans;
-    scanf("%d %d %d", &N, &L, &R);
-    int A[N];
+int N, L, R;
+vector<int> A(N);
 
+/**
+ * @brief ・Aiがl以上R未満のときはAi
+ * ・AiがL未満のときはL
+ * ・AiがR以上のときはR
+*/
+static void minimize_abc() {
     for (int i = 0; i < N; i++) {
-        scanf("%d", &A[i]);
-
-        if (abs(L - A[i]) < abs(R - A[i])) {
-            ans = L;
-        } else {
-            ans = R;
+        if (L <= A[i] && A[i] < R) {
+            cout << A[i] << endl;
+        } else if (A[i] < L) {
+            cout << L << endl;
+        } else if (R <= A[i]) {
+            cout << R << endl;
         }
-        
-        printf("%d ", ans);
     }
+}
+
+int main() {
+    cin >> N >> L >> R;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    minimize_abc();
     
     return 0;
 }
