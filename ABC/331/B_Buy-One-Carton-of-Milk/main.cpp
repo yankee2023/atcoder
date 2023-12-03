d5c922f748c21d06cc910f0111e35a399470ddd1
yankee <tad.training2023@gmail.com>
2023-12-03 14:36:37 +0900

    331_B解き直し

--- ABC/331/B_Buy-One-Carton-of-Milk/main.cpp
+++ ABC/331/B_Buy-One-Carton-of-Milk/main.cpp
@@ -5,10 +5,14 @@ int main() {
     int N, S, M, L;
     cin >> N >> S >> M >> L;
 
+    /*
+        卵の個数が100なので全探索してもTLEにはならない
+        最大計算量は10^6
+    */
     int ans = INT32_MAX;
-    for (int i = 0; 6*i <= N; i++) {
-        for (int j = 0; 8*j <= N-6*i; j++) {
-            for (int k = 0; 12*k <= (N-6*i-8*j); k++) {
+    for (int i = 0; i <= 100; i++) {
+        for (int j = 0; j <= 100; j++) {
+            for (int k = 0; k <= 100; k++) {
                 if (6*i + 8*j + 12*k >= N) {
                     ans = min((i*S + j*M + k*L), ans);
                 }
