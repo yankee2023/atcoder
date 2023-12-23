c4a584d31ff4189d30a4e5fe0a67f5d0ebd66a93
yankee <tad.training2023@gmail.com>
2023-12-23 22:40:17 +0900

    フォルダ名変更とD問題考え方記載

--- ABC/334/D_Reindeer-and-Sleigh/main.cpp
+++ ABC/334/D_Reindeer-and-Sleigh/main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    int N, Q;
+    int N, Q;               // ソリの数とクエリの個数
     cin >> N >> Q;
-    vector<int> R(N);
+    vector<int> R(N);       // ソリを引くのに必要なトナカイ
     int query[Q];
     for (int i = 0; i < N; i++) {
         cin >> R[i];
@@ -17,6 +17,12 @@ int main()
     // 昇順にソート
     sort(R.begin(), R.end());
 
+/*  考え方
+    トナカイ全匹から、
+    昇順にソートしたソリを引くのに必要なトナカイRi匹を
+    引いていけば答えになると思った。
+
+*/
     for (int i = 0; i < Q; i++) {
         int ans = 0;
         for (int j = 0; j < N; j++) {
