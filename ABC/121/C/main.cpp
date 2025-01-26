528e11c50cb5b7bb1bead0c2483b6655dbdac6e3
yankee <tad.training2023@gmail.com>
2025-01-26 23:47:13 +0900

    ABC121

---
+++ ABC/121/C/main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)n; ++i)
+constexpr int INF = 1 << 30;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    vector<pair<ll, ll>> ab(N);
+    rep(i, N) cin >> ab[i].first >> ab[i].second;
+
+    ll ans = 0;
+    sort(ab.begin(), ab.end());
+    for (int i = 0; i < N; i++)
+    {
+        if (ab[i].second > M) {
+            ans += ab[i].first * M;
+            break;
+        } else {
+            ans += ab[i].second * ab[i].first;
+            M -= ab[i].second;
+        }
+    }
+    
+    cout << ans << endl;
+    return 0;
+}
