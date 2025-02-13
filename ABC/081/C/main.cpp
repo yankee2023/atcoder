9e47205f8c73632e2cb43150e698d72c387c0cd5
yankee <tad.training2023@gmail.com>
2025-02-13 22:30:36 +0900

    ABC115

--- ABC/081/C/main.cpp
+++ ABC/081/C/main.cpp
@@ -1,12 +1,6 @@
 #include <algorithm>
-#include <cmath>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <set>
 #include <string>
-#include <tuple>
 #include <vector>
 
 using namespace std;
