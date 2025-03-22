03405a9f87cd3202943ac685ad6d35d78d91cc2b
yankee <tad.training2023@gmail.com>
2025-03-22 17:08:22 +0900

    template修正
    includeの並び順、const intへ変更

--- ABC/template/A/main.cpp
+++ ABC/template/A/main.cpp
@@ -1,18 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <tuple>
 #include <algorithm>
 #include <cmath>
 #include <iomanip>
-#include <iostream>
-#include <map>
 #include <queue>
-#include <set>
-#include <string>
-#include <tuple>
-#include <vector>
 
 using namespace std;
 using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)n; ++i)
-constexpr int INF = 1 << 30;
+const int INF = 1 << 30;
 
 int main()
 {
