eeeb5bc3e563b44a764b3aabf5b8f924f716a240
yankee <tad.training2023@gmail.com>
2025-05-06 11:59:57 +0900

    ABC404 C,D

--- ABC/404/C/main.cpp
+++ ABC/404/C/main.cpp
@@ -14,11 +14,14 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)n; ++i)
 const int INF = 1 << 30;
 
-int main()
+/**
+ * @brief 問題文から素直に解く
+ */
+void solve1()
 {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> graph(m);
+    vector<vector<int>> graph(n);
     rep(i,m) {
         int a, b;
         cin >> a >> b;
@@ -27,12 +30,117 @@ int main()
         graph[b].push_back(a);
     }
 
-    rep(i,m) {
+    // 次数が2かの判定
+    rep(i,n) {
         if (2 == graph[i].size()) continue;
         cout << "No" << endl;
-        return 0;
+        return;
     }
 
+    // 全頂点を辿って戻ってこれるかの判定
+    vector<bool> visited(n, false);
+    int pre = 0, now = graph[0][0];
+    rep(i,n) {  // サイクルとは頂点=辺の数
+        if (visited[now]) { // 探索済み頂点に再度来たら終了
+            cout << "No" << endl;
+            return;
+        }
 
+        visited[now] = true;
+        int next = 0;
+        rep(i,2) {  // 探索済み頂点でない頂点へ進む
+            if (graph[now][i] != pre) next = graph[now][i];
+        }
+        pre = now;
+        now = next;
+    }
+
+    rep(i,n) {
+        if (!visited[i]) {
+            cout << "No" << endl;
+            return;
+        }
+    }
+
+    cout << "Yes" << endl;
+}
+
+struct UnionFind
+{
+    /**
+     * @brief 親の頂点番号とサイズを同時に管理する。
+     * 根ではない頂点のparは親の頂点が格納している。
+     * 根にはサイズ数が格納している。
+     * 両者を区別するためサイズはマイナス値で入っている。
+     */
+    vector<int> par;
+
+    /**
+     * @brief コンストラクタ
+     * -1で初期化
+     * @param n 全頂点数
+     */
+    UnionFind(int n) : par(n, -1) { }
+
+    int root(int x)
+    {
+        // 負の値の場合、根である
+        if (par[x] < 0) return x;
+        return par[x] = root(par[x]);
+    }
+
+    bool merge(int x, int y)
+    {
+        int rx = root(x);
+        int ry = root(y);
+        if (rx == ry) return false;
+        if (par[rx] > par[ry]) swap(rx, ry);
+        par[rx] += par[ry];
+        par[ry] = rx;
+        return true;
+    }
+
+    bool same(int x, int y)
+    {
+        return root(x) == root(y);
+    }
+
+    int size(int x)
+    {
+        return -par[root(x)];
+    }
+};
+
+/**
+ * @brief サイクルグラフの性質から解く
+ * ・次数が2
+ * ・連結
+ */
+void solve_unionFind()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> deg(n); // 次数
+    UnionFind uf(n);
+    rep(i,m) {
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        deg[a]++;
+        deg[b]++;
+        uf.merge(a, b);
+    }
+
+    // 判定
+    if (deg == vector<int>(n,2) && uf.size(0) == n) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    solve_unionFind();
     return 0;
 }
--- ABC/404/D/main.cpp
+++ ABC/404/D/main.cpp
@@ -14,7 +14,7 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (int)n; ++i)
 const int INF = 1001001001;
 
-int main()
+void solve1()
 {
     int n, m;
     cin >> n >> m;
@@ -59,5 +59,53 @@ int main()
     }
 
     cout << ans*2 << endl;
+}
+
+/**
+ * @brief 各動物園に訪れる回数を3進数で表現
+ * 
+ */
+void solve_ans()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> c(n);
+    rep(i,n) cin >> c[i];
+    vector<vector<int>> a(m);
+    rep(i,m) {
+        int k;
+        cin >> k;
+        a[i] = vector<int>(k);
+        rep(j,k) {
+            cin >> a[i][j];
+            a[i][j]--;
+        }
+    }
+
+    // 3のべき乗を求めておく
+    vector<int> p3(n+1,1);
+    rep(i,n) p3[i+1] = p3[i]*3;
+
+    const ll inf = 1e18;
+    ll ans = inf;
+    rep(s,p3[n]) {  // 末尾が3のn乗となる
+        vector<int> t(n);   // 各動物園へ訪れる回数
+        rep(i,n) t[i] = s/p3[i]%3;  // i桁目が取れる
+        ll cost = 0;
+        rep(i,n) cost += c[i]*t[i];
+        rep(j,m) {
+            int cnt = 0;
+            for (int i : a[j]) cnt += t[i];
+            if (cnt < 2) cost = inf;
+        }
+        ans = min(ans, cost);
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    solve_ans();
     return 0;
 }
