e6d7bf82c26a379c00b4550249801272de9dc446
yankee <tad.training2023@gmail.com>
2025-06-28 19:08:20 +0900

    ABC147

---
+++ ABC/147/C/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <queue>
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)n; ++i)
+const int INF = 1 << 30;
+
+int main()
+{
+    int n;
+    cin >> n;
+    rep(i,n) {
+        int a;
+        cin >> a;
+        rep(j,a) {
+            int x, y;
+            cin >> x >> y;
+        }
+    }
+    return 0;
+}
