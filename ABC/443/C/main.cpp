452b9020a97ece3527ba53ef8cea8141e41be6cf
yankee <tad.training2023@gmail.com>
2026-02-08 15:49:42 +0900

    ABC 443

---
+++ ABC/443/C/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <queue>
+
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)n; ++i)
+const int INF = 1 << 30;
+
+int main()
+{
+    
+    return 0;
+}
