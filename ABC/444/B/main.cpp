a89c0d7178d4df1e422bbb5865d69f9122294890
yankee <tad.training2023@gmail.com>
2026-02-17 23:49:41 +0900

    ABC444 B問題

--- ABC/444/B/main.cpp
+++ ABC/444/B/main.cpp
@@ -16,6 +16,22 @@ const int INF = 1 << 30;
 
 int main()
 {
-    
+    int N, K;
+    cin >> N >> K;
+    int sum = 0, ans = 0;
+
+    for (int i = 1; i <= N; ++i) {
+        int temp = i;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (sum == K) {
+            ans++;
+        }
+        sum = 0;
+    }
+
+    cout << ans << endl;
     return 0;
 }
